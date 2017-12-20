cdbf90ae989c0c67a44720a13575ec88196d7b74
Hai <hainv@arrowhitech.com>
2017-12-20 10:42:17 +0700

    update

--- sites/all/themes/flatastic/templates/view/clients/views-view--clients--client-home-2.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php print $header;?>
-<div class="product_brands with_sidebar m_sm_bottom_35">
-
-  <?php print $rows;?>
-</div>
--- sites/all/themes/flatastic/templates/view/products/views-view--product-specials--block.tpl.php
+++
@@ -1,9 +0,0 @@
-<figure class="widget shadow r_corners wrapper m_bottom_30">
-    <?php print $header;?>
-    <div class="widget_content">
-        <div class="specials_carousel">
-            <!--carousel item-->
-            <?php print $rows;?>
-        </div>
-    </div>
-</figure>
